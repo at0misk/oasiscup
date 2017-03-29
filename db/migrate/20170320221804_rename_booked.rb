cd3f259b8c56ef9ebd5c4927601343c5887d5630
at0misk <ktp925@gmail.com>
2017-03-29 08:18:33 -0700

    fixed migrations

--- db/migrate/20170320221804_rename_booked.rb
+++ db/migrate/20170320221804_rename_booked.rb
@@ -1,5 +1,5 @@
 class RenameBooked < ActiveRecord::Migration[5.0]
   def change
-     rename_table :booked, :books
+     rename_table :booked_rooms, :books
   end
 end
--- db/migrate/20170320221948_rename_booked_again.rb
+++
@@ -1,5 +0,0 @@
-class RenameBookedAgain < ActiveRecord::Migration[5.0]
-  def change
-     rename_table :booked, :books
-  end
-end
