fd14ca77fb06f6fadd31dc66441e114518c6d602
at0misk <ktp925@gmail.com>
2017-03-29 09:34:43 -0700

    initial stripe setup

--- app/controllers/charges_controller.rb
+++ app/controllers/charges_controller.rb
@@ -14,5 +14,20 @@ class ChargesController < ApplicationController
 	    :description => 'Rails Stripe customer',
 	    :currency    => 'usd'
 	  )
+	  	@user = User.find(session[:user_id])
+		@cart = Cart.where(user_id: session[:user_id])
+		@cart.each do |val|
+			@booked = Book.new
+			@booked.hotel_id = val.hotel_id
+			@booked.user_id = val.user_id
+			@booked.price = val.price
+			@booked.number = val.number
+			@booked.smoking = val.smoking
+			@booked.room_type = val.room_type
+			@booked.save
+			Room.where(hotel_id: val.hotel_id, number: val.number).destroy_all
+			Cart.where(user_id: session[:user_id]).destroy_all
+		end
+		redirect_to '/booked'
 	end
 end
