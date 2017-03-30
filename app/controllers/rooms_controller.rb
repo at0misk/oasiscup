e765656c2f226e16984c4f967bf9b7144e8be1cc
at0misk <ktp925@gmail.com>
2017-03-30 11:46:47 -0700

    bracket searching now runs to all

--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -25,33 +25,48 @@ class RoomsController < ApplicationController
       @cart.each do |val|
         @cartNumberArr << val.number
       end
-      if session[:searchingAll] == true
-        @rooms = @@roomSwitch
-        # puts '============'
-        # puts @rooms.first
-        # fail
+      if session[:price_range]
+        if session[:price_range] == 1
+            @rooms = Room.where(price: 70..120).order(:price)
+        elsif session[:price_range] == 2
+            @rooms = Room.where(price: 120..150).order(:price)
+        elsif session[:price_range] == 3
+            @rooms = Room.where(price: 150..200).order(:price)
+        end
+        session[:price_range] = nil
       else
-        @rooms = Room.all
-        # if @user.guests != nil
-        #   @user.guests.each do |val|
-        #     if val.guest_type == "Child"
-        #       session[:childCount] += 1
-        #     elsif val.guest_type == "Adult"
-        #       session[:adultCount] += 1
-        #     end
-        #   end
-        # end
+        if session[:searchingAll] == true
+          @rooms = @@roomSwitch
+          # puts '============'
+          # puts @rooms.first
+          # fail
+        else
+          @rooms = Room.all
+          # if @user.guests != nil
+          #   @user.guests.each do |val|
+          #     if val.guest_type == "Child"
+          #       session[:childCount] += 1
+          #     elsif val.guest_type == "Adult"
+          #       session[:adultCount] += 1
+          #     end
+          #   end
+          # end
+        end
+        session[:searchingAll] = false
       end
-      session[:searchingAll] = false
   	end
   	def search
   		if params[:id] == '1'
-  			@rooms = Room.where(price: 70..120).order(:price)
+  			# @rooms = Room.where(price: 70..120).order(:price)
+        session[:price_range] = 1
   		elsif params[:id] == '2'
-  			@rooms = Room.where(price: 120..150).order(:price)
+  			# @rooms = Room.where(price: 120..150).order(:price)
+          session[:price_range] = 2
   		elsif params[:id] == '3'
-  			@rooms = Room.where(price: 150..200).order(:price)
+  			# @rooms = Room.where(price: 150..200).order(:price)
+        session[:price_range] = 3
   		end
+      redirect_to '/rooms'
   	end
   	def count
   		session[:fromCount] = true
@@ -108,5 +123,5 @@ class RoomsController < ApplicationController
         # session[:searchingAll] = false
       end
       redirect_to :back
-    end
+      end
 end
