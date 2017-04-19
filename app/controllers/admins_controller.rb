e0224ed95f6a22e352cf1ee60d9deff5266f559c
at0misk <ktp925@gmail.com>
2017-04-19 13:35:28 -0700

    admin verification

--- app/controllers/admins_controller.rb
+++ app/controllers/admins_controller.rb
@@ -1,22 +1,18 @@
 class AdminsController < ApplicationController
 	def dash
-		if !session[:user_id]
-			redirect_to '/'
-		else
-			@user = User.find(session[:user_id])
-			if !@user.permod
-				redirect_to '/'
-			end
-		end
+		verifyAdmin
 	end
 	def searchUsers
+		verifyAdmin
 		@users = User.where('email LIKE :search OR first LIKE :search OR last LIKE :search', search: "%#{@@params}%")
 	end
 	def searchUsersSearch
+		verifyAdmin
 		@@params = params['user']
 		redirect_to '/admins/searchUsers'
 	end
 	def searchTransactions
+		verifyAdmin
 		if @@code != ''
 			@transaction = Transaction.find_by(transaction_code: "#{@@code}")
 			@user = User.find(@transaction.user_id)
@@ -26,12 +22,23 @@ class AdminsController < ApplicationController
 		end
 	end
 	def searchTransactionsSearch
+		verifyAdmin
 		if params['code']
 			@@code = params['code']
 			redirect_to '/admins/searchTransactions'
 		else
 			flash[:errors] = "Must be a valid confirmation number"
 			redirect_to :back
+		end
+	end
+	def verifyAdmin
+		if !session[:user_id]
+			redirect_to '/'
+		else
+			@user = User.find(session[:user_id])
+			if !@user.permod
+				redirect_to '/'
+			end
 		end
 	end
 end
