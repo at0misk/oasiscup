0b6563b77f33eaf5f52529c4471e26ad35fcd723
at0misk <ktp925@gmail.com>
2017-04-25 15:41:05 -0700

    auth tests

--- app/controllers/payments_controller.rb
+++ app/controllers/payments_controller.rb
@@ -14,15 +14,14 @@ class PaymentsController < ApplicationController
   # POST
   # Returns relay response when Authorize.Net POSTs to us.
   def relay_response
-      puts("Processing..")
-      sim_response = AuthorizeNet::SIM::Response.new(params)
-      if sim_response.success?('9CPC3p3r8J', 'PBDGMKX')
-        puts "wat"
-        fail
-        render :text => sim_response.direct_post_reply(payments_receipt_url(:only_path => false), :include => true)
-      else
-        render
-      end
+      # puts("Processing..")
+      # sim_response = AuthorizeNet::SIM::Response.new(params)
+      # if sim_response.success?('9CPC3p3r8J', 'PBDGMKX')
+      #   render :text => sim_response.direct_post_reply(payments_receipt_url(:only_path => false), :include => true)
+      # else
+      #   render
+      # end
+      redirect_to '/'
   end
   
   # GET
