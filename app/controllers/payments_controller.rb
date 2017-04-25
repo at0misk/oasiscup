a2e28314b79bcf5466d1d3ab02382dd5c0429247
at0misk <ktp925@gmail.com>
2017-04-25 10:56:49 -0700

    auth net

--- app/controllers/payments_controller.rb
+++ app/controllers/payments_controller.rb
@@ -2,7 +2,7 @@ class PaymentsController < ApplicationController
 require 'digest/md5'
   # layout 'authorize_net'
   helper :authorize_net
-  protect_from_forgery :except => :relay_response
+  # protect_from_forgery :except => :relay_response
 
   # GET
   # Displays a payment form.
@@ -17,7 +17,7 @@ require 'digest/md5'
   # POST
   # Returns relay response when Authorize.Net POSTs to us.
   def relay_response
-    layout false
+    # layout false
     @hash = Digest::MD5.hexdigest('pbdg0245' + '9CPC3p3r8J' + '2RV3fr4sBsf7995S' + '10.00').upcase
     sim_response = AuthorizeNet::SIM::Response.new(params)
     if sim_response.success?('9CPC3p3r8J', 'pbdg0245')
