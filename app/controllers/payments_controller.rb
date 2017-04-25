18aeb3670d6ed203bca48e278934ef9d47512fad
at0misk <ktp925@gmail.com>
2017-04-25 14:38:07 -0700

    auth tests

--- app/controllers/payments_controller.rb
+++ app/controllers/payments_controller.rb
@@ -2,7 +2,7 @@ class PaymentsController < ApplicationController
 
   # layout 'authorize_net'
   helper :authorize_net
-  protect_from_forgery :except => :relay_response
+  # protect_from_forgery :except => :relay_response
 
   # GET
   # Displays a payment form.
