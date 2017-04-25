90c645889ea9a956ece31a16ea9e9d173e2dabd5
at0misk <ktp925@gmail.com>
2017-04-25 13:34:06 -0700

    auth tests

--- app/controllers/payments_controller.rb
+++ app/controllers/payments_controller.rb
@@ -1,38 +1,31 @@
 class PaymentsController < ApplicationController
-require 'digest/md5'
-  # layout 'authorize_net'
+
+  layout 'authorize_net'
   helper :authorize_net
-  # protect_from_forgery :except => :relay_response
+  protect_from_forgery :except => :relay_response
 
   # GET
   # Displays a payment form.
   def payment
     @amount = 10.00
-    @sim_transaction = AuthorizeNet::SIM::Transaction.new('9CPC3p3r8J', '2RV3fr4sBsf7995S', @amount, :relay_url => payments_relay_response_url(:only_path => false))
-    # puts "======"
-    # puts @sim_transaction.fingerprint
-    # fail
+    @sim_transaction = AuthorizeNet::SIM::Transaction.new(AUTHORIZE_NET_CONFIG['api_login_id'], AUTHORIZE_NET_CONFIG['api_transaction_key'], @amount, :relay_url => payments_relay_response_url(:only_path => false))
   end
 
   # POST
   # Returns relay response when Authorize.Net POSTs to us.
   def relay_response
-    # layout false
     sim_response = AuthorizeNet::SIM::Response.new(params)
-    # @hash = Digest::MD5.hexdigest('PBDGMKX' + '9CPC3p3r8J' + "#{@hash.x_trans_id}" + "#{@hash.x_amount}").upcase
-    if sim_response
+    if sim_response.success?(AUTHORIZE_NET_CONFIG['api_login_id'], AUTHORIZE_NET_CONFIG['merchant_hash_value'])
       render :text => sim_response.direct_post_reply(payments_receipt_url(:only_path => false), :include => true)
     else
-      # @success = sim_response.success?('9CPC3p3r8J', 'pbdg0245')
       render
     end
   end
-
+  
   # GET
   # Displays a receipt.
   def receipt
     @auth_code = params[:x_auth_code]
-    # redirect_to 'http://www.google.com'
   end
 
 end
