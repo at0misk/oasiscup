23f1e2a5c4a26b011617bc447730f8d7475ba24a
at0misk <ktp925@gmail.com>
2017-04-25 14:06:51 -0700

    auth tests

--- app/controllers/payments_controller.rb
+++ app/controllers/payments_controller.rb
@@ -8,7 +8,7 @@ class PaymentsController < ApplicationController
   # Displays a payment form.
   def payment
     @amount = 10.00
-    @sim_transaction = AuthorizeNet::SIM::Transaction.new('9CPC3p3r8J', '85GL7ApYu8v533sU', @amount, :relay_url => payments_relay_response_url(:only_path => false))
+    @sim_transaction = AuthorizeNet::SIM::Transaction.new('9CPC3p3r8J', '85GL7ApYu8v533sU', @amount, :relay_url => 'http://developer.authorize.net/bin/developer/paramdump')
   end
 
   # POST
