a56a41dce0ada2267e6dd0d90cbbb358472b365a
at0misk <ktp925@gmail.com>
2017-04-26 08:09:12 -0700

    auth

--- app/controllers/payments_controller.rb
+++ app/controllers/payments_controller.rb
@@ -8,14 +8,12 @@ class PaymentsController < ApplicationController
   # Displays a payment form.
   def payment
     @amount = 10.00
-    @sim_transaction = AuthorizeNet::SIM::Transaction.new('9CPC3p3r8J', '85GL7ApYu8v533sU', @amount, :relay_url => "http://requestb.in/umhbh5um")
+    @sim_transaction = AuthorizeNet::SIM::Transaction.new('9CPC3p3r8J', '85GL7ApYu8v533sU', @amount, :relay_url => "http://www.oasiscup.com/payments/relay_response")
   end
 
   # POST
   # Returns relay response when Authorize.Net POSTs to us.
   def relay_response
-      puts("Processing..")
-      print("whatat")
       sim_response = AuthorizeNet::SIM::Response.new(params)
       if sim_response.success?('9CPC3p3r8J', 'PBDGMKX')
         render :text => sim_response.direct_post_reply(payments_receipt_url(:only_path => false), :include => true)
