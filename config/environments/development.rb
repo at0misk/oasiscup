e7b455da53eaf2e0553333f1fe8e487ed76474eb
at0misk <ktp925@gmail.com>
2017-09-11 14:27:49 -0700

    fe

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -34,7 +34,7 @@ Rails.application.configure do
    :address              => "smtp.gmail.com",
    :port                 => 587,
    :user_name            => "oasiscuppalmdesert",
-   :password             => "billions0925",
+   :password             => "Pbdg01350",
    :authentication       => "plain",
   :enable_starttls_auto => true
   }
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -62,7 +62,7 @@ Rails.application.configure do
    :address              => "smtp.gmail.com",
    :port                 => 587,
    :user_name            => "oasiscuppalmdesert",
-   :password             => "billions0925",
+   :password             => "Pbdg01350",
    :authentication       => "plain",
   :enable_starttls_auto => true
   }
