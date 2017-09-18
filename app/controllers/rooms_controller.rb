351a8859c943b36b8b74c7e81b3f95e0eb4b5230
at0misk <ktp925@gmail.com>
2017-09-18 08:30:37 -0700

    mailer

--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -20,13 +20,13 @@ class RoomsController < ApplicationController
       if !@user.permod
       else
         x = 1
-        30.times do
+        1.times do
           @room = Room.new
           @room.number = x
           @room.hotel_id = params[:id]
           @room.smoking = "No"
           @room.room_type = "Double Queens"
-          @room.price = 259.00
+          @room.price = 165.00
           @room.occupancy_a = 4
           # @room.occupancy_c = 
           @room.description = ""
